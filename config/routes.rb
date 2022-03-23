ac2d743f48408fb23d827a33639f63cee7faa149
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-22 19:25:28 -0600

    add route for /ski_makers/:id in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/ski_makers', to: 'ski_makers#index'
+  get '/ski_makers/:id', to: 'ski_makers#show'
 end
