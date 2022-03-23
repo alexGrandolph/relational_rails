d3477e1fad8c62dba816446ed7edd179decb888a
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-22 19:33:28 -0600

    Test: rename first test in show_spec.rb to be more accurate

--- spec/features/ski_makers/show_spec.rb
+++ spec/features/ski_makers/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'the ski_makers shows page' do
 
-  it 'displays the attributes for the ski_maker using id as path' do
+  it 'displays new page for specific ski_maker :id' do
     ski_maker1 = SkiMaker.create(company_name: "4frnt", years_active: 15, makes_snowboards: false)
     visit "/ski_makers/#{ski_maker1.id}"
 
