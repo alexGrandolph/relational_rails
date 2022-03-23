f9d30e85dc566d805fea710f0daa464c2fae618d
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-23 14:43:45 -0600

    Test: refactor features/ski_makers/show_spec.rb to test first user story

--- spec/features/ski_makers/show_spec.rb
+++ spec/features/ski_makers/show_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'the ski_makers shows page' do
+RSpec.describe 'the /ski_makers page' do
 
-  it 'displays new page for specific ski_maker :id' do
-    ski_maker1 = SkiMaker.create(company_name: "4frnt", years_active: 15, makes_snowboards: false)
-    visit "/ski_makers/#{ski_maker1.id}"
+  it '/ski_makers displays the name for each ski_makers' do
+    ski_maker1 = SkiMaker.create(company_name: "4frnt", years_active: 13, makes_snowboards: false)
+    ski_maker2 = SkiMaker.create(company_name: "Line", years_active: 15, makes_snowboards: false)
+    ski_maker3 = SkiMaker.create(company_name: "Salomon", years_active: 35, makes_snowboards: true)
+    ski_maker3 = SkiMaker.create(company_name: "1000 Skis", years_active: 2, makes_snowboards: false)
 
+    visit "/ski_makers/"
+    expect(page).to have_content(ski_maker1.company_name)
+    expect(page).to have_content(ski_maker2.company_name)
+    expect(page).to have_content(ski_maker3.company_name)
+    expect(page).to have_content(ski_maker4.company_name)
   end
 end
