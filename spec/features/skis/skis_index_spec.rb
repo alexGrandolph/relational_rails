ee4fee6c0fd8cf9b93889693a354470e86830560
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-24 16:02:30 -0600

    Test: add test for button link to skis#index on skis#show and skis#index pages

--- spec/features/skis/skis_index_spec.rb
+++ spec/features/skis/skis_index_spec.rb
@@ -22,4 +22,17 @@ RSpec.describe 'the /skis index page' do
     expect(page).to have_content(ski_2.symmetrical)
   end
 
+  it 'has clickable link to skis#index' do
+    icelantic = SkiMaker.create!(company_name: "Icelantic", years_active: 15, makes_snowboards: false)
+    nomad = icelantic.skis.create!(model: "Nomad", ski_type: "Park", longest_offered_cm: 191, symmetrical: true)
+    shaman = icelantic.skis.create!(model: "Shaman", ski_type: "Powder", longest_offered_cm: 209, symmetrical: false)
+    madien = icelantic.skis.create!(model: "Madien", ski_type: "Park", longest_offered_cm: 178, symmetrical: true)
+    saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
+
+    visit "/skis/"
+    click_on 'See All Skis!'
+    save_and_open_page
+    expect(current_path).to eq("/skis/")
+  end
+
 end
