dd536eb7415b14c75b1297fd732fb7bf767033aa
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-24 15:47:07 -0600

    Test: add test for ski_makers#index page displaying in order by created_at and displaying created_at for each ski_maker

--- spec/features/ski_makers/index_spec.rb
+++ spec/features/ski_makers/index_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe 'the /ski_makers index page' do
     expect(page).to have_content(ski_maker3.company_name)
     expect(page).to have_content(ski_maker4.company_name)
   end
+
+  it 'displays ski_makers in order by created_at and shows created at on page' do
+    faction = SkiMaker.create!(company_name: "Faction", years_active: 13, makes_snowboards: false)
+    agent = faction.skis.create!(model: "Agent", ski_type: "Park", longest_offered_cm: 195, symmetrical: true)
+    prodigy = faction.skis.create!(model: "Prodigy", ski_type: "Park", longest_offered_cm: 198, symmetrical: true)
+    ct = faction.skis.create!(model: "CT 2.0", ski_type: "Backcountry", longest_offered_cm: 213, symmetrical: false)
+
+    visit "/ski_makers/"
+    save_and_open_page
+
+    expect(page).to have_content("Added to Site: #{faction.created_at}")
+
+  end
 end
