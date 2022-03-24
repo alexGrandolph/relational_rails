454e2ba06df8659590e057716f6e7fb6b1383580
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-24 15:55:54 -0600

    Test: add test for clickable button to go to skis#index on ski_makers#index page

--- spec/features/ski_makers/index_spec.rb
+++ spec/features/ski_makers/index_spec.rb
@@ -23,9 +23,23 @@ RSpec.describe 'the /ski_makers index page' do
     ct = faction.skis.create!(model: "CT 2.0", ski_type: "Backcountry", longest_offered_cm: 213, symmetrical: false)
 
     visit "/ski_makers/"
-    save_and_open_page
+    # save_and_open_page
 
     expect(page).to have_content("Added to Site: #{faction.created_at}")
-
   end
+
+  it 'has clickable link to skis#index' do
+    icelantic = SkiMaker.create!(company_name: "Icelantic", years_active: 15, makes_snowboards: false)
+    nomad = icelantic.skis.create!(model: "Nomad", ski_type: "Park", longest_offered_cm: 191, symmetrical: true)
+    shaman = icelantic.skis.create!(model: "Shaman", ski_type: "Powder", longest_offered_cm: 209, symmetrical: false)
+    madien = icelantic.skis.create!(model: "Madien", ski_type: "Park", longest_offered_cm: 178, symmetrical: true)
+    saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
+
+    visit "/ski_makers/"
+    click_on 'See All Skis!'
+
+    expect(current_path).to eq("/skis/")
+  end
+
+
 end
