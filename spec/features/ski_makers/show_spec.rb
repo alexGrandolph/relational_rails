53950323d79cea2f6a7e2b3ef6a85618152d970e
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-24 18:04:49 -0600

    Test: add test for link to a ski_makers skis from their :id show page

--- spec/features/ski_makers/show_spec.rb
+++ spec/features/ski_makers/show_spec.rb
@@ -52,5 +52,18 @@ RSpec.describe 'the /ski_makers/:id show page' do
     expect(current_path).to eq("/ski_makers/")
   end
 
+  it 'has clickable link to ski_makers#index' do
+    icelantic = SkiMaker.create!(company_name: "Icelantic", years_active: 15, makes_snowboards: false)
+    nomad = icelantic.skis.create!(model: "Nomad", ski_type: "Park", longest_offered_cm: 191, symmetrical: true)
+    shaman = icelantic.skis.create!(model: "Shaman", ski_type: "Powder", longest_offered_cm: 209, symmetrical: false)
+    madien = icelantic.skis.create!(model: "Madien", ski_type: "Park", longest_offered_cm: 178, symmetrical: true)
+    saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
+
+    visit "/ski_makers/#{icelantic.id}"
+    click_on 'See These Skis!'
+
+    expect(current_path).to eq("/ski_makers/:id/skis")
+  end
+
 
 end
