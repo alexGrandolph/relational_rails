47b56039a7088f8342647af86818c27b96d5c4f1
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-26 15:48:57 -0600

    Feat: add route for ski_maker/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
+
   get '/ski_makers', to: 'ski_makers#index'
   get '/ski_makers/new', to: 'ski_makers#new'
   post '/ski_makers', to: 'ski_makers#create'
   get '/ski_makers/:id', to: 'ski_makers#show'
+  get '/ski_makers/:id/edit', to: 'ski_makers#edit'
   get '/skis', to: 'skis#index'
   get '/skis/:id', to: 'skis#show'
   get '/ski_makers/:id/skis', to: 'ski_makers#skis'
