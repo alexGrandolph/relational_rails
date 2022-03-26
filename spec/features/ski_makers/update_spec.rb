ee6ebae2b4fa9eeb36b3abe21da96ca6d7661e9a
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-26 16:09:09 -0600

    Feat: add form to :id/edit, refactor test to only test for seeing form, filling out form will be a new test

--- spec/features/ski_makers/update_spec.rb
+++ spec/features/ski_makers/update_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'can update a ski_maker(parent)' do
 
     end
 
-    it 'can fill the form to update/edit a ski_maker' do
+    it 'can see the form to update/edit a ski_maker' do
       line = SkiMaker.create!(company_name: "Line", years_active: 15, makes_snowboards: false)
       blade = line.skis.create!(model: "BLADE", ski_type: "Powder", longest_offered_cm: 215, symmetrical: false)
       tom = line.skis.create!(model: "Tom Wallisch Pro", ski_type: "Park", longest_offered_cm: 205, symmetrical: true)
@@ -26,15 +26,17 @@ RSpec.describe 'can update a ski_maker(parent)' do
       visit "ski_makers/#{line.id}"
       click_on "Edit This Brand"
 
-      fill_in 'Company Name', with: 'Line Skis'
-      fill_in 'Years Active', with: 22
-      fill_in 'Do They Make Snowboards? (true or false)', with: false
+      # fill_in 'Company Name:', with: 'Line Skis'
+      # fill_in 'Years Active:', with: 22
+      # fill_in 'Do They Make Snowboards? (true or false):', with: false
+      save_and_open_page
 
       expect(current_path).to eq("/ski_makers/#{line.id}/edit")
-      expect(page).to have_content("Line Skis")
-
-
+      expect(page).to have_content('Company Name:')
+      expect(page).to have_content('Years Active:')
+      expect(page).to have_content('Do They Make Snowboards? (true or false):')
     end
+
 
 
 
