504aa8575f967b33928a5abd65990d361072e6db
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-26 16:13:39 -0600

    Feat: add PATCH route from ski_maker/edit page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post '/ski_makers', to: 'ski_makers#create'
   get '/ski_makers/:id', to: 'ski_makers#show'
   get '/ski_makers/:id/edit', to: 'ski_makers#edit'
+  patch '/ski_makers/:id', to: 'ski_makers#update'
   get '/skis', to: 'skis#index'
   get '/skis/:id', to: 'skis#show'
   get '/ski_makers/:id/skis', to: 'ski_makers#skis'
