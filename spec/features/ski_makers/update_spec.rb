c4127c479464e016c023c7ccb5acc5b1f1dd7643
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-26 15:58:28 -0600

    Test: add test for seeing a form on :id/edit and filling in the form

--- spec/features/ski_makers/update_spec.rb
+++ spec/features/ski_makers/update_spec.rb
@@ -17,6 +17,25 @@ RSpec.describe 'can update a ski_maker(parent)' do
 
     end
 
+    it 'can fill the form to update/edit a ski_maker' do
+      line = SkiMaker.create!(company_name: "Line", years_active: 15, makes_snowboards: false)
+      blade = line.skis.create!(model: "BLADE", ski_type: "Powder", longest_offered_cm: 215, symmetrical: false)
+      tom = line.skis.create!(model: "Tom Wallisch Pro", ski_type: "Park", longest_offered_cm: 205, symmetrical: true)
+      chronic = line.skis.create!(model: "Chronic", ski_type: "Park", longest_offered_cm: 204, symmetrical: true)
+
+      visit "ski_makers/#{line.id}"
+      click_on "Edit This Brand"
+
+      fill_in 'Company Name', with: 'Line Skis'
+      fill_in 'Years Active', with: 22
+      fill_in 'Do They Make Snowboards? (true or false)', with: false
+
+      expect(current_path).to eq("/ski_makers/#{line.id}/edit")
+      expect(page).to have_content("Line Skis")
+
+
+    end
+
 
 
 
