e37d3db12976185e5c7cf519ce8880f2275c5c06
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-27 13:30:01 -0600

    Feat: add framework for form on /skis/new page, need to add a tie to parent

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get '/ski_makers/:id/skis/new', to: 'ski_makers#new_ski'
   get '/ski_makers', to: 'ski_makers#index'
   get '/ski_makers/new', to: 'ski_makers#new'
   post '/ski_makers', to: 'ski_makers#create'
@@ -8,6 +9,5 @@ Rails.application.routes.draw do
   patch '/ski_makers/:id', to: 'ski_makers#update'
   get '/skis', to: 'skis#index'
   get '/skis/:id', to: 'skis#show'
-  get '/ski_makers/:id/skis/new', to: 'ski_makers#new_ski'
   get '/ski_makers/:id/skis', to: 'ski_makers#skis'
 end
