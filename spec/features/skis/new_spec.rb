8af2b89e46b55a382a2e7362e7c648c0c5162b56
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-27 13:06:44 -0600

    Test: add test for clicking on create new skis link and current path being ski_makers/:id/skis/new

--- spec/features/skis/new_spec.rb
+++ spec/features/skis/new_spec.rb
@@ -11,8 +11,27 @@ RSpec.describe 'New Ski Maker' do
       saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
 
       visit "/ski_makers/#{icelantic.id}/skis"
-      save_and_open_page
+      # save_and_open_page
       expect(page).to have_content("Create Skis")
+    end
+
+    it 'can click the link to create new skis and is taken to a /new page' do
+
+      icelantic = SkiMaker.create!(company_name: "Icelantic", years_active: 15, makes_snowboards: false)
+      nomad = icelantic.skis.create!(model: "Nomad", ski_type: "Park", longest_offered_cm: 191, symmetrical: true)
+      shaman = icelantic.skis.create!(model: "Shaman", ski_type: "Powder", longest_offered_cm: 209, symmetrical: false)
+      madien = icelantic.skis.create!(model: "Madien", ski_type: "Park", longest_offered_cm: 178, symmetrical: true)
+      saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
+
+      visit "/ski_makers/#{icelantic.id}/skis"
+
+      click_on "Create Skis"
+      
+      expect(current_path).to eq("/ski_makers/#{icelantic.id}/skis/new")
+
+
+
+
 
     end
 
