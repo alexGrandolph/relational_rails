02d022775458d0910be1fb1eac2937a6e205b68a
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-27 13:45:55 -0600

    Test: add test for filling in and submiting new skis form

--- spec/features/skis/new_spec.rb
+++ spec/features/skis/new_spec.rb
@@ -38,11 +38,38 @@ RSpec.describe 'New Ski Maker' do
       saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
 
       visit "/ski_makers/#{icelantic.id}/skis/new"
-      save_and_open_page
+      # save_and_open_page
       expect(page).to have_content("Model Name:")
       expect(page).to have_content("Ski Type:")
       expect(page).to have_content("Longest Offered Ski (cm):")
       expect(page).to have_content("Is This Ski 100% Symmetrical (true or false):")
+
+    end
+
+    it 'can fill in and submit/create new pair of skis' do
+
+      icelantic = SkiMaker.create!(company_name: "Icelantic", years_active: 15, makes_snowboards: false)
+      nomad = icelantic.skis.create!(model: "Nomad", ski_type: "Park", longest_offered_cm: 191, symmetrical: true)
+      shaman = icelantic.skis.create!(model: "Shaman", ski_type: "Powder", longest_offered_cm: 209, symmetrical: false)
+      madien = icelantic.skis.create!(model: "Madien", ski_type: "Park", longest_offered_cm: 178, symmetrical: true)
+      saba = icelantic.skis.create!(model: "Saba", ski_type: "All Mountain", longest_offered_cm: 201, symmetrical: false)
+
+      visit "/ski_makers/#{icelantic.id}/skis/new"
+
+      fill_in 'Model Name:', with: 'Pioneer'
+      # fill_in "Ski Type:", with: "All Mountain"
+      fill_in 'Longest Offered Ski (cm):', with: 184
+      fill_in 'Is This Ski 100% Symmetrical (true or false):', with: false
+      click_on 'Create Skis'
+      save_and_open_page
+
+      expect(current_path).to eq("/ski_makers/#{icelantic.id}/skis")
+      expect(page).to have_content("Pioneer")
+
+
+
+
+
 
     end
 
