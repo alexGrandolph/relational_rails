eb899155300d524ffd746c467ea11b8795e8391c
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-27 15:24:52 -0600

    Feat: add #update action in skis controller

--- app/controllers/skis_controller.rb
+++ app/controllers/skis_controller.rb
@@ -12,4 +12,16 @@ class SkisController < ApplicationController
     @ski = Ski.find(params[:id])
   end
 
+  def update
+    updated_ski = Ski.find(params[:id])
+    updated_ski.update({
+      model: params[:ski][:model],
+      ski_type: params[:ski][:ski_type],
+      longest_offered_cm: params[:ski][:longest_offered_cm],
+      symmetrical: params[:ski][:symmetrical]
+      })
+    updated_ski.save
+    redirect_to "/skis/#{updated_ski.id}"
+  end
+
 end
