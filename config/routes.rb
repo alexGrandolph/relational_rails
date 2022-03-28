2a79c7a34a19923c294c775c01c11bf2961d715b
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-28 17:46:35 -0600

    Feat: add delete route for /ski_makers/:id to ski_makers#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/ski_makers/new', to: 'ski_makers#new'
   post '/ski_makers', to: 'ski_makers#create'
   get '/ski_makers/:id', to: 'ski_makers#show'
+  delete '/ski_makers/:id', to: 'ski_makers#destroy'
   get '/ski_makers/:id/edit', to: 'ski_makers#edit'
   patch '/ski_makers/:id', to: 'ski_makers#update'
 
